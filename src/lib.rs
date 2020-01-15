fed85f165288b9ec25b6b9f5b6252ae9778f5e4c
Steven Ireland <scientastersteve@gmail.com>
2020-01-15 08:18:08 -0500

    add tests to double check trig, fix 4th quadrant bug

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,59 @@
 mod world;
+
+use std::f64::consts::PI;
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+  #[test]
+  fn test_angles_on_axis() {
+    let p1 = world::Point {x: 0.0, y: 0.0};
+    
+    // Edge cases
+    let p2 = world::Point {x: 0.0, y: 1.0};
+    assert_eq!(PI / 2.0, world::get_force_angle(p1, p2));
+
+    let p2 = world::Point {x: -1.0, y: 0.0};
+    assert_eq!(PI, world::get_force_angle(p1, p2));
+
+    let p2 = world::Point {x: 0.0, y: -1.0};
+    assert_eq!(PI + PI / 2.0, world::get_force_angle(p1, p2));
+
+    let p2 = world::Point {x: 1.0, y: 0.0};
+    assert_eq!(0.0, world::get_force_angle(p1, p2));
+  }
+
+  #[test]
+  fn test_45_degree_angles() {
+    let p1 = world::Point {x: 0.0, y: 0.0};
+    
+    let p2 = world::Point {x: 1.0, y: 1.0};
+    assert_eq!(PI / 4.0, world::get_force_angle(p1, p2));
+
+    let p2 = world::Point {x: -1.0, y: 1.0};
+    assert_eq!(PI - PI / 4.0, world::get_force_angle(p1, p2));
+
+    let p2 = world::Point {x: -1.0, y: -1.0};
+    assert_eq!(PI + PI / 4.0, world::get_force_angle(p1, p2));
+
+    let p2 = world::Point {x: 1.0, y: -1.0};
+    assert_eq!(2.0 * PI - PI / 4.0, world::get_force_angle(p1, p2));
+  }
+
+  #[test]
+  fn test_offset_angles() {
+    let p1 = world::Point {x: 2.0, y: 2.0};
+    
+    let p2 = world::Point {x: 3.0, y: 3.0};
+    assert_eq!(PI / 4.0, world::get_force_angle(p1, p2));
+
+    let p2 = world::Point {x: 1.0, y: 3.0};
+    assert_eq!(PI - PI / 4.0, world::get_force_angle(p1, p2));
+
+    let p2 = world::Point {x: 1.0, y: 1.0};
+    assert_eq!(PI + PI / 4.0, world::get_force_angle(p1, p2));
+
+    let p2 = world::Point {x: 3.0, y: 1.0};
+    assert_eq!(2.0 * PI - PI / 4.0, world::get_force_angle(p1, p2));
+  }
+}
